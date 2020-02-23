66899b0650a8c8da316639513d5d4f29ae3d274d
Mitch Hussey <mitchhussey@gmail.com>
2020-02-22 23:55:09 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,40 @@
+require(`dotenv`).config({path: `.env`,})
+
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Forty V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Forty by HTML5 UP"
+    title: "HeatherShober.com",
+    author: "Mitch Hussey",
+    description: "Memorial website for Heather Hussey Shober"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'HeatherShober.com',
+        short_name: 'Heather Shober',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#1d2135',
+        theme_color: '#1d2135',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+      },
+    },
+
+    
     'gatsby-plugin-sass',
     'gatsby-plugin-offline'
   ],
